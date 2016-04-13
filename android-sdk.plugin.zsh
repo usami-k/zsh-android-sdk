790f56f65e97b0c49340fe5f9bac39b789079baf
USAMI Kosuke <usami.kosuke@gmail.com>
2016-04-13 10:17:01 +0900

    Use zsh Glob Qualifiers

--- android-sdk.plugin.zsh
+++ android-sdk.plugin.zsh
@@ -1,6 +1,4 @@
 () {
     local android_sdk="$HOME/Library/Android/sdk"
-    if [ -d $android_sdk ]; then
-        path=($path $android_sdk/platform-tools $android_sdk/tools)
-    fi
+    path=($path $android_sdk/platform-tools(N-/) $android_sdk/tools(N-/))
 }
